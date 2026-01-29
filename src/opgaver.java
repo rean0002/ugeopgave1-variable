9e6bb8dfed6462adde7914e2bda50ce8aa84f47e
rean0002 <rean0002@stud.kea.dk>
2026-01-29 15:04:15 +0100

    opgave 1-6

---
+++ src/opgaver.java
@@ -0,0 +1,76 @@
+
+void main() {
+    //Opgave 1
+    /*
+    double item1 = 25.50;
+    double item2 = 15.00;
+    double item3 = 30.00;
+
+    double subtotal = item1 + item2 + item3;
+
+    double tax = 25.00;
+
+    double withTax = subtotal + (subtotal * tax /100.0);
+
+    System.out.println(subtotal);
+    System.out.println(withTax);*/
+
+    //Opgave 2
+/*
+    double food = 450.0;
+    double drinks = 120.0;
+    double tips = 50.0;
+    int peopleAmount = 4;
+    double total = food + drinks + tips;
+    double perPerson = total / peopleAmount;
+
+    System.out.println(perPerson);
+*/
+    //Opgave 3
+/*
+    double originalPrice = 300.0;
+    double discountRate = 20.0;
+    double discountPrice = originalPrice - (originalPrice * discountRate /100.0);
+    double taxRate = 25.0;
+    double totalPrice = discountPrice + (discountPrice * taxRate/100.0);
+
+    System.out.println(originalPrice);
+    System.out.println(discountPrice);
+    System.out.println(totalPrice);
+*/
+    //Opgave 4
+/*
+    int guests = 15;
+    int cupcakes = 40;
+    int cupcakePerPerson = cupcakes/guests;
+    int cupcakeLeftOver = cupcakes % guests;
+    System.out.println(cupcakePerPerson);
+    System.out.println(cupcakeLeftOver);
+ */
+    //Opgave 5
+/*
+    StringBuilder sb = new StringBuilder();
+    String name = "Anna";
+    int age = 25;
+    sb.append ("Hello, ");
+    sb.append (name);
+    sb.append ("!");
+    sb.append (",");
+    sb.append ("You are ");
+    sb.append (age);
+    sb.append (" years old.");
+
+    System.out.println(sb.toString());
+*/
+    //Opgave 6
+
+    int score = 100;
+    System.out.println(score);
+    score += 50;
+    System.out.println(score);
+    score += 25;
+    System.out.println(score);
+
+
+
+}
